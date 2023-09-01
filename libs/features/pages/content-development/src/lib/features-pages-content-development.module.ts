7c2fc1fc2130b1326f6f01e74de7a6c8b05747a2
Tracie35 <kariukitracie@gmail.com>
2023-09-01 15:52:03 +0300

    :sprinkle:generated-content-dev-hero-section-component

--- libs/features/pages/content-development/src/lib/features-pages-content-development.module.ts
+++ libs/features/pages/content-development/src/lib/features-pages-content-development.module.ts
@@ -4,10 +4,14 @@ import { CommonModule } from '@angular/common';
 import { ContentDevelopmentPageComponent } from './main/content-development-page/content-development-page.component';
 
 import { ContentDevelopmentPageRoutingModule } from './content-development.routing';
+import { ContentDevHeroSectionComponent } from './components/content-dev-hero-section/content-dev-hero-section.component';
 
 @NgModule({
   imports: [CommonModule, ContentDevelopmentPageRoutingModule],
-  declarations: [ContentDevelopmentPageComponent],
-  exports: [ContentDevelopmentPageComponent]
+  declarations: [
+    ContentDevelopmentPageComponent,
+    ContentDevHeroSectionComponent,
+  ],
+  exports: [ContentDevelopmentPageComponent],
 })
 export class ContentDevelopmentModule {}
